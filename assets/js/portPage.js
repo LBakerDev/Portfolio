9edd61389ea57a65201e3a143a6770dbc57bf438
londonbaker4@gmail.com <londonbaker4@gmail.com>
2017-08-06 19:46:06 -0400

    complete version

---
+++ assets/js/portPage.js
@@ -0,0 +1,47 @@
+// Document ready shorthand statement
+    // $(function() {
+    //   // Select link by id and add click event
+    //   $('#about').click(function() {
+
+    //     // Animate Scroll to #bottom
+    //     $('html,body').animate({
+    //       scrollTop: $("#about").offset().top }, // Tell it to scroll to the top #bottom
+    //       1000 // How long scroll will take in milliseconds
+    //     );
+
+    //     // Prevent default behavior of link
+    //     return false;
+    //   });
+    // });
+
+    // Document ready shorthand statement
+    $(function() {
+      // Select link by id and add click event
+      $('#projects').click(function() {
+
+        // Animate Scroll to #bottom
+        $('html,body').animate({
+          scrollTop: $("#projects").offset().top }, // Tell it to scroll to the top #bottom
+          1000 // How long scroll will take in milliseconds
+        );
+
+        // Prevent default behavior of link
+        return false;
+      });
+    });
+
+    // Document ready shorthand statement
+    $(function() {
+      // Select link by id and add click event
+      $('#contact').click(function() {
+
+        // Animate Scroll to #bottom
+        $('html,body').animate({
+          scrollTop: $("#contact").offset().top }, // Tell it to scroll to the top #bottom
+          1000 // How long scroll will take in milliseconds
+        );
+
+        // Prevent default behavior of link
+        return false;
+      });
+    });
